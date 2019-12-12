c230a054cdfbcf8567f8caaf0ab5c41457b2b5be
MuhammadYamalAmilun <49560796+ReinChan@users.noreply.github.com>
2019-12-12 20:00:30 +0700

    Add files via upload

---
+++ yo.py
@@ -0,0 +1,187 @@
+invar = 2
+turn = "x"
+a = 8
+b = 0
+c = 0
+d = 0
+e = 0
+f = 0
+g = 0
+h = 0
+i = 0
+def main():
+    print("|"+str(a)+"|"+str(b)+"|"+str(c)+"|")
+    print("|"+str(d)+"|"+str(e)+"|"+str(f)+"|")
+    print("|"+str(g)+"|"+str(h)+"|"+str(i)+"|")
+main()
+npush = input(turn + " Turn : ")
+
+if npush == "a":
+    if a == int(a) and turn == "x":
+        a += 1
+        turn = "o"
+    elif a == int(a) and turn == "o":
+        int(a)
+        a -= 1
+        str(a)
+        turn = "x"
+    elif a == str(a) and turn == "o":
+        int(a)
+        a += 1
+        str(a)
+        turn = "x"
+    else:
+        int(a)
+        a -= 1
+        turn = "o"
+
+if npush == "b":
+    if b == int() and turn == "x":
+        b += 1
+        turn = "o"
+    elif b == int(b) and turn == "o":
+        int(b)
+        b -= 1
+        str(b)
+        turn = "x"
+    elif b == str(b) and turn == "o":
+        int(b)
+        b += 1
+        str(b)
+        turn = "x"
+    else:
+        int(b)
+        b -= 1
+        turn = "o"        
+if npush == "c":
+    if c == int(c) and turn == "x":
+        c += 1
+        turn = "o"
+    elif c == int(c) and turn == "o":
+        int(c)
+        c -= 1
+        str(c)
+        turn = "x"
+    elif c == str(c) and turn == "o":
+        int(c)
+        c += 1
+        str(c)
+        turn = "x"
+    else:
+        int(c)
+        c -= 1
+        turn = "o"
+
+if npush == "d":
+    if d == int(d) and turn == "x":
+        d += 1
+        turn = "o"
+    elif d == int(d) and turn == "o":
+        int(d)
+        d -= 1
+        str(d)
+        turn = "x"
+    elif d == str(d) and turn == "o":
+        int(d)
+        d += 1
+        str(d)
+        turn = "x"
+    else:
+        int(a)
+        a -= 1
+        turn = "o"        
+
+if npush == "e":
+    if e == int(e) and turn == "x":
+        e += 1
+        turn = "o"
+    elif e == int(e) and turn == "o":
+        int(e)
+        e -= 1
+        str(e)
+        turn = "x"
+    elif e == str(e) and turn == "o":
+        int(e)
+        e += 1
+        str(e)
+        turn = "x"
+    else:
+        int(e)
+        e -= 1
+        turn = "o"
+  
+if npush == "f":
+    if f == int(f) and turn == "x":
+        f += 1
+        turn = "o"
+    elif f == int(f) and turn == "o":
+        int(f)
+        f -= 1
+        str(f)
+        turn = "x"
+    elif f == str(f) and turn == "o":
+        int(f)
+        f += 1
+        str(f)
+        turn = "x"
+    else:
+        int(f)
+        f -= 1
+        turn = "o"
+
+if npush == "g":
+    if g == int(g) and turn == "x":
+        g += 1
+        turn = "o"
+    elif g == int(g) and turn == "o":
+        int(g)
+        g -= 1
+        str(g)
+        turn = "x"
+    elif g == str(g) and turn == "o":
+        int(g)
+        g += 1
+        str(g)
+        turn = "x"
+    else:
+        int(g)
+        g -= 1
+        turn = "o"
+
+if npush == "g":
+    if g == int(g) and turn == "x":
+        g += 1
+        turn = "o"
+    elif g == int(g) and turn == "o":
+        int(g)
+        g -= 1
+        str(g)
+        turn = "x"
+    elif g== str(g) and turn == "o":
+        int(g)
+        g += 1
+        str(g)
+        turn = "x"
+    else:
+        int(g)
+        g -= 1
+        turn = "o"
+
+if npush == "a":
+    if a == int(a) and turn == "x":
+        a += 1
+        turn = "o"
+    elif a == int(a) and turn == "o":
+        int(a)
+        a -= 1
+        str(a)
+        turn = "x"
+    elif a == str(a) and turn == "o":
+        int(a)
+        a += 1
+        str(a)
+        turn = "x"
+    else:
+        int(a)
+        a -= 1
+        turn = "o"
